89545e596a7b4309b1a6e4cf54d44c78c139a24f
vandrecx <vcortesoliveira@gmail.com>
2022-10-05 17:13:22 -0300

    D3tec

---
+++ site cadastro/cad.php
@@ -0,0 +1,20 @@
+<?php
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$dbname = "vascodagama";
+
+//Conexao
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+$result_usuario = "INSERT INTO user (nome, email, senha) VALUES ('$nome','$email','$senha')";
+$resultado_usuario = mysqli_query($conn, $result_usuario);
+
+echo "<br>o Nome e ".$nome;
+echo "<br>o Email e ".$email;
+echo "<br>a Senha e ".$senha;
+?>
