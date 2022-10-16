2720a83c30f1e380777e4b22dbe92b3201f6e450
Magnus Felinto <magnusfelinto@gmail.com>
2022-10-16 20:04:17 -0300

    .

--- shrek/update.php
+++ shrek/update.php
@@ -8,6 +8,6 @@
     $sql1 = "UPDATE roteiro SET titulo = '$ti', subtitulo ='$sub', autor = '$autor', texto = '$t' WHERE id = 1";
     //$resu =  mysqli_query($conn, $sql1);
     $conn->query($sql1);
-    echo "GDS";
+
 
 ?>
