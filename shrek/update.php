d813ea3199407c94ce2a05f3c78b8ace137f6179
vandrecx <vcortesoliveira@gmail.com>
2022-10-16 19:31:59 -0300

    Alteração temporária

--- shrek/update.php
+++ shrek/update.php
@@ -5,6 +5,7 @@
     $subtitulo  = $_POST['subtitulo'];
     $texto      = $_POST['texto'];
 
-    $sql1 = "UPDATE roteiro SET titulo='$titulo', subtitulo='$subtitulo', texto='$texto' WHERE id=1";
+    $sql1 = "INSERT INTO roteiro (titulo, subtitulo, texto) VALUES ('$titulo', '$subtitulo', '$texto')";
     $resu = mysqli_query($conn, $sql1);
+
 ?>
